22ea50da23020ba08b9b7026d47922e79fcc5c18
vento <capoeiravenezuela@gmail.com>
2020-08-13 17:44:21 -0400

    new

---
+++ index.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+
+(async () =>{
+    let movieUrl = "https://www.imdb.com/title/tt0111161/?ref_=nv_sr_srsg_0";
+    let browser = await puppeteer.launch({headless: false});
+    let page = await browser.newPage();
+    await page.goto(movieUrl, {waitUntil: 'networkidle2'});
+    let data = await page.evaluate(()=> {
+        let title = document.querySelector('div[class="title_wrapper"] > h1').innerText;
+        let rating = document.querySelector('span[itemprop="ratingValue"]').innerText;
+        let ratingCount = document.querySelector('span[itemprop="ratingCount"]').innerText;
+
+        return{
+            title,
+            rating,
+            ratingCount
+        }
+    });
+    console.log(data);
+    debugger;
+})();
